31957bcb96d0615cc451d3b0952ba8c3836e1c5a
Danil2537 <dedov7347@gmail.com>
2025-08-18 20:03:53 +0300

    deploy stuff

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors({ origin: process.env.FRONTEND_ORIGIN?.split(',') || true, credentials: true }));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-
+  app.enableCors({
+    origin: process.env.FRONTEND_ORIGIN?.split(',') || true,
+    credentials: true,
+  });
   const port = process.env.PORT || 3001;
   await app.listen(port);
   console.log(`API listening on ${port}`);
